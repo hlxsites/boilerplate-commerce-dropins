28d95246c5ebb9d789eda83e9449d198345de92c
Carlos A. Cabrera <cacabrer@adobe.com>
2024-03-13 14:55:19 -0500

    hmm

--- scripts/__dropins__/storefront-pdp/432.js
+++ scripts/__dropins__/storefront-pdp/432.js
@@ -17,14 +17,13 @@ export const modules = {
 
 // EXPORTS
 __webpack_require__.d(__webpack_exports__, {
-  fH: () => (/* binding */ IntlContext),
   Pj: () => (/* binding */ IntlProvider),
   Cf: () => (/* binding */ Localizer),
   xv: () => (/* binding */ Text),
   XK: () => (/* binding */ useText)
 });
 
-// UNUSED EXPORTS: MarkupText, default, intl, translate, withText
+// UNUSED EXPORTS: IntlContext, MarkupText, default, intl, translate, withText
 
 // EXTERNAL MODULE: external "@dropins/tools/preact.js"
 var preact_js_ = __webpack_require__(770);
