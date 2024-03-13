829fe8bbd330e2bb427b97165be9968de5c33eb8
Carlos A. Cabrera <cacabrer@adobe.com>
2024-03-13 15:24:45 -0500

    test

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -40,96 +40,96 @@ export default async function decorate(block) {
 
   return productRenderer.render(ProductDetails, {
     sku: getSkuFromUrl(),
-    carousel: {
-      controls: 'dots', // 'thumbnailsColumn', 'thumbnailsRow', 'dots'
-      // mobile: true,
-    },
-    slots: {
-      Title: (ctx) => {
-        // Title Decoration
-        const decoration = document.createElement('div');
-        decoration.classList.add('title-decoration');
-        decoration.innerHTML = 'WKND Essentials';
+    // carousel: {
+    //   controls: 'dots', // 'thumbnailsColumn', 'thumbnailsRow', 'dots'
+    //   // mobile: true,
+    // },
+    // slots: {
+    //   Title: (ctx) => {
+    //     // Title Decoration
+    //     const decoration = document.createElement('div');
+    //     decoration.classList.add('title-decoration');
+    //     decoration.innerHTML = 'WKND Essentials';
 
-        ctx.prependSibling(decoration);
+    //     ctx.prependSibling(decoration);
 
-        // Rating
-        const ratings = document.createElement('div');
-        ratings.classList.add('ratings');
-        ratings.innerHTML = '⭐️⭐️⭐️⭐️';
+    //     // Rating
+    //     const ratings = document.createElement('div');
+    //     ratings.classList.add('ratings');
+    //     ratings.innerHTML = '⭐️⭐️⭐️⭐️';
 
-        ctx.appendSibling(ratings);
-      },
-      Quantity: (ctx) => {
-        const label = document.createElement('div');
-        label.classList.add('quantity-label');
-        label.innerHTML = 'Quantity';
+    //     ctx.appendSibling(ratings);
+    //   },
+    //   Quantity: (ctx) => {
+    //     const label = document.createElement('div');
+    //     label.classList.add('quantity-label');
+    //     label.innerHTML = 'Quantity';
 
-        ctx.prependChild(label);
+    //     ctx.prependChild(label);
 
-        const promo = document.createElement('div');
-        promo.classList.add('quantity-promo');
-        promo.innerHTML = 'Buy 3 or more to get a free month of WKND+';
+    //     const promo = document.createElement('div');
+    //     promo.classList.add('quantity-promo');
+    //     promo.innerHTML = 'Buy 3 or more to get a free month of WKND+';
 
-        ctx.appendChild(promo);
-      },
-      Actions: (ctx) => {
-        // Add to Cart Button
-        ctx.appendButton((next, state) => {
-          const adding = state.get('adding');
-          return {
-            text: adding ? 'Adding to Cart' : 'Add to Cart',
-            icon: 'Cart',
-            variant: 'primary',
-            disabled: adding || !next.data.inStock,
-            onClick: async () => {
-              try {
-                state.set('adding', true);
+    //     ctx.appendChild(promo);
+    //   },
+    //   Actions: (ctx) => {
+    //     // Add to Cart Button
+    //     ctx.appendButton((next, state) => {
+    //       const adding = state.get('adding');
+    //       return {
+    //         text: adding ? 'Adding to Cart' : 'Add to Cart',
+    //         icon: 'Cart',
+    //         variant: 'primary',
+    //         disabled: adding || !next.data.inStock,
+    //         onClick: async () => {
+    //           try {
+    //             state.set('adding', true);
 
-                if (!next.valid) {
-                  // eslint-disable-next-line no-console
-                  console.warn('Invalid product', next.values);
-                  return;
-                }
+    //             if (!next.valid) {
+    //               // eslint-disable-next-line no-console
+    //               console.warn('Invalid product', next.values);
+    //               return;
+    //             }
 
-                await addProductsToCart([{ ...next.values }]);
-              } catch (error) {
-                // eslint-disable-next-line no-console
-                console.warn('Error adding product to cart', error);
-              } finally {
-                state.set('adding', false);
-              }
-            },
-          };
-        });
+    //             await addProductsToCart([{ ...next.values }]);
+    //           } catch (error) {
+    //             // eslint-disable-next-line no-console
+    //             console.warn('Error adding product to cart', error);
+    //           } finally {
+    //             state.set('adding', false);
+    //           }
+    //         },
+    //       };
+    //     });
 
-        ctx.appendButton((next, state) => {
-          return {
-            text: 'Like',
-            icon: 'Heart',
-            variant: 'secondary',
-            disabled: true,
-          };
-        });
+    //     ctx.appendButton((next, state) => {
+    //       return {
+    //         text: 'Like',
+    //         icon: 'Heart',
+    //         variant: 'secondary',
+    //         disabled: true,
+    //       };
+    //     });
 
-        // Promo Link
-        const promoLink = document.createElement('a');
-        promoLink.classList.add('quantity-banner');
-        promoLink.innerHTML = 'Get Free Month Subscription';
-        promoLink.setAttribute('href', '#');
+    //     // Promo Link
+    //     const promoLink = document.createElement('a');
+    //     promoLink.classList.add('quantity-banner');
+    //     promoLink.innerHTML = 'Get Free Month Subscription';
+    //     promoLink.setAttribute('href', '#');
 
-        ctx.appendChild(promoLink);
+    //     ctx.appendChild(promoLink);
 
-        ctx.onChange(next => {
-          const valid = next.values.quantity >= 3;
+    //     ctx.onChange(next => {
+    //       const valid = next.values.quantity >= 3;
 
-          if (valid) {
-            promoLink.style.display = 'block';
-          } else {
-            promoLink.style.display = 'none';
-          }
-        });
-      },
-    },
+    //       if (valid) {
+    //         promoLink.style.display = 'block';
+    //       } else {
+    //         promoLink.style.display = 'none';
+    //       }
+    //     });
+    //   },
+    // },
   })(block);
 }
