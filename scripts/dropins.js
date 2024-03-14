d1f314de7da308f88d2c1cbf0c64ebdf5e54ae29
Carlos A. Cabrera <cacabrer@adobe.com>
2024-03-14 13:21:32 -0500

    disabled logger

--- scripts/dropins.js
+++ scripts/dropins.js
@@ -35,5 +35,5 @@ export default async function initializeDropins() {
     window.addEventListener('load', initializers.mount);
   }
 
-  events.enableLogger(true);
+  // events.enableLogger(true);
 }
