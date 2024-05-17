87024a9c6cd9be42091b92fe263adeb4d0acfdc1
Carlos A. Cabrera <cacabrer@adobe.com>
2024-05-17 13:57:22 -0500

    test

--- scripts/__dropins__/tools/components/Icon.js
+++
@@ -1,2 +0,0 @@
-import{I}from"../chunks/index.js";import"../preact-compat.js";import"../preact.js";import"../preact-hooks.js";import"../preact-jsx-runtime.js";export{I as Icon};
-//# sourceMappingURL=Icon.js.map
