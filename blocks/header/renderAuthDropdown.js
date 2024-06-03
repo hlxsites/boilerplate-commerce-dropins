d35beb9323a0b1950cd36a4c5de1ca19ad615057
Konstantin Fandelyuk <k.fandeliuk@atwix.com>
2024-06-04 00:04:33 +0300

    eds-232 - updated  boilerplate integration

--- blocks/header/renderAuthDropdown.js
+++ blocks/header/renderAuthDropdown.js
@@ -37,19 +37,20 @@ function renderSignIn(element) {
     },
     formSize: 'small',
     routeForgotPassword: () => '/customer/forgotpassword',
-    successNotificationForm: (userName) => h(SuccessNotification, {
-      headingText: `Welcome ${userName}`,
-      messageText: 'You have successfully logged in.',
-      primaryButtonText: 'My Account',
-      secondaryButtonText: 'Logout',
-      onPrimaryButtonClick: () => {
-        window.location.href = '/customer/account';
-      },
-      onSecondaryButtonClick: async () => {
-        await authApi.revokeCustomerToken();
-        window.location.href = '/';
-      },
-    }),
+    successNotificationForm: (userName) =>
+      h(SuccessNotification, {
+        headingText: `Welcome ${userName}`,
+        messageText: 'You have successfully logged in.',
+        primaryButtonText: 'My Account',
+        secondaryButtonText: 'Logout',
+        onPrimaryButtonClick: () => {
+          window.location.href = '/customer/account';
+        },
+        onSecondaryButtonClick: async () => {
+          await authApi.revokeCustomerToken();
+          window.location.href = '/';
+        },
+      }),
   })(wrapperBlock);
 
   element.appendChild(wrapperBlock);
@@ -69,7 +70,7 @@ function getPopupElements() {
   };
 }
 
-function renderPopupContent(isAuthenticated, popupElements) {
+export function renderPopupContent(isAuthenticated, popupElements) {
   const { headerLoginButton, popupElement, popupMenuContainer } = popupElements;
 
   popupMenuContainer.innerHTML = '';
@@ -78,7 +79,8 @@ function renderPopupContent(isAuthenticated, popupElements) {
     popupElement.style.minWidth = '250px';
 
     if (headerLoginButton) {
-      headerLoginButton.textContent = `Hello, ${getCookie('auth_dropin_firstname')}`;
+      const spanElementText = headerLoginButton.querySelector('span');
+      spanElementText.textContent = `Hi, ${getCookie('auth_dropin_firstname')}`;
     }
 
     popupMenuContainer.insertAdjacentHTML(
@@ -141,13 +143,9 @@ export const renderAuthDropdown = (navSectionsEl) => {
 
   navSectionsEl.insertAdjacentHTML(
     'afterend',
-    `<div class="wrapperPopUpButton" id="wrapper-pop-up-button">
+    `<div class="section wrapperPopUpButton" id="wrapper-pop-up-button">
       <button type="button" id="header-login-button">
-        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path vector-effect="non-scaling-stroke" d="M11.8052 14.4968C10.8552 14.4968 9.9752 14.0268 9.4452 13.2368L9.4152 13.1868L9.3852 13.1268C8.1352 11.2268 7.5352 8.96681 7.6852 6.68681C7.7552 4.42681 9.6052 2.61681 11.8652 2.60681H12.0052C14.2752 2.47681 16.2152 4.21681 16.3452 6.47681C16.3452 6.55681 16.3452 6.62681 16.3452 6.70681C16.4852 8.94681 15.9052 11.1768 14.6852 13.0568L14.6052 13.1768C14.0552 13.9868 13.1352 14.4668 12.1652 14.4768H12.0052C11.9352 14.4768 11.8652 14.4868 11.7952 14.4868L11.8052 14.4968Z" stroke="currentColor"/>
-        <path vector-effect="non-scaling-stroke" d="M4.3252 21.5469C4.3552 20.4169 4.4752 19.2869 4.6752 18.1769C4.8952 17.1669 6.4752 16.0269 8.9052 15.1569C9.2352 15.0369 9.4852 14.7869 9.5952 14.4569L9.8052 14.0269" stroke="currentColor"/>
-        <path vector-effect="non-scaling-stroke" d="M14.425 14.4069L14.165 14.1569C14.375 14.5969 14.725 14.9569 15.155 15.1869C16.945 15.7969 19.125 16.9569 19.375 18.2069C19.585 19.3069 19.685 20.4269 19.675 21.5369" stroke="currentColor"/>
-        </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" class="small-icon-login-user"><g fill="none" stroke="#000000" stroke-width="1.5"><circle cx="12" cy="6" r="4"></circle><path d="M20 17.5c0 2.485 0 4.5-8 4.5s-8-2.015-8-4.5S7.582 13 12 13s8 2.015 8 4.5Z"></path></g></svg>
       <span>Sign in</span>
       </button>
       <div id="modalHeaderContainer">
@@ -155,8 +153,8 @@ export const renderAuthDropdown = (navSectionsEl) => {
           <div class="popupMenuContent">
             <div class="popupMenuContainer">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" class="iconClosePopUp ${
-  isAuthenticated ? 'isHidden' : ''
-}">
+                isAuthenticated ? 'isHidden' : ''
+              }">
                 <path vector-effect="non-scaling-stroke" d="M18.3599 5.64001L5.62988 18.37" stroke="currentColor"/>
                 <path vector-effect="non-scaling-stroke" d="M18.3599 18.37L5.62988 5.64001" stroke="currentColor"/>
               </svg>
@@ -177,20 +175,18 @@ export const renderAuthDropdown = (navSectionsEl) => {
   }
 };
 
-events.on(
-  'authenticated',
-  (isAuthenticated) => {
-    if (!isAuthenticated) {
-      const headerLoginButton = document.getElementById('header-login-button');
-      const popupMenu = document.querySelector('.popupMenu');
-      const popupMenuUrlList = document.querySelector('.popupMenuUrlList');
-      const popupMenuContainer = document.querySelector(
-        '.popupMenu > .popupMenuContent .popupMenuContainer',
-      );
+events.on('authenticated', (isAuthenticated) => {
+  if (!isAuthenticated) {
+    const headerLoginButton = document.getElementById('header-login-button');
+    const popupMenu = document.querySelector('.popupMenu');
+    const popupMenuUrlList = document.querySelector('.popupMenuUrlList');
+    const popupMenuContainer = document.querySelector(
+      '.popupMenu > .popupMenuContent .popupMenuContainer',
+    );
 
-      popupMenuContainer.innerHTML = '';
+    popupMenuContainer.innerHTML = '';
 
-      headerLoginButton.innerHTML = `
+    headerLoginButton.innerHTML = `
         <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path vector-effect="non-scaling-stroke" d="M11.8052 14.4968C10.8552 14.4968 9.9752 14.0268 9.4452 13.2368L9.4152 13.1868L9.3852 13.1268C8.1352 11.2268 7.5352 8.96681 7.6852 6.68681C7.7552 4.42681 9.6052 2.61681 11.8652 2.60681H12.0052C14.2752 2.47681 16.2152 4.21681 16.3452 6.47681C16.3452 6.55681 16.3452 6.62681 16.3452 6.70681C16.4852 8.94681 15.9052 11.1768 14.6852 13.0568L14.6052 13.1768C14.0552 13.9868 13.1352 14.4668 12.1652 14.4768H12.0052C11.9352 14.4768 11.8652 14.4868 11.7952 14.4868L11.8052 14.4968Z" stroke="currentColor"/>
             <path vector-effect="non-scaling-stroke" d="M4.3252 21.5469C4.3552 20.4169 4.4752 19.2869 4.6752 18.1769C4.8952 17.1669 6.4752 16.0269 8.9052 15.1569C9.2352 15.0369 9.4852 14.7869 9.5952 14.4569L9.8052 14.0269" stroke="currentColor"/>
@@ -199,11 +195,10 @@ events.on(
         <span>Sign In</span>
       `;
 
-      popupMenuUrlList.remove();
+    popupMenuUrlList.remove();
 
-      popupMenu.style.minWidth = '330px';
+    popupMenu.style.minWidth = '330px';
 
-      renderSignIn(popupMenuContainer);
-    }
-  },
-);
+    renderSignIn(popupMenuContainer);
+  }
+});
