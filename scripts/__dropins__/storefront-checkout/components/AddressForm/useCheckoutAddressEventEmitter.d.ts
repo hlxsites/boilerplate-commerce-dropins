2f4443e40fca36c64fef56557cde906dec857569
Natxo Cabré <cabre@adobe.com>
2024-08-09 16:06:05 +0200

    removed checkout dropins script files

--- scripts/__dropins__/storefront-checkout/components/AddressForm/useCheckoutAddressEventEmitter.d.ts
+++
@@ -1,9 +0,0 @@
-import { AddressFormType, AddressFormValues } from '../../data/models';
-
-type useCheckoutAddressEventEmitterProps = {
-    address: AddressFormValues;
-    type: AddressFormType;
-};
-declare function useCheckoutAddressEventEmitter({ address, type, }: useCheckoutAddressEventEmitterProps): void;
-export default useCheckoutAddressEventEmitter;
-//# sourceMappingURL=useCheckoutAddressEventEmitter.d.ts.map
