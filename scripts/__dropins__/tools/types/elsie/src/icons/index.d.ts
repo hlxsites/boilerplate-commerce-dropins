7d0a48984910da4a1c00fa3422b1b252a7d69b2c
Abrasimov Yaroslav <y.abrasimov@atwix.com>
2024-08-12 09:48:17 +0200

    Apply ReCaptcha fix for PSI metric (2.0)

--- scripts/__dropins__/tools/types/elsie/src/icons/index.d.ts
+++ scripts/__dropins__/tools/types/elsie/src/icons/index.d.ts
@@ -35,4 +35,5 @@ export { default as Trash } from './Trash.svg';
 export { default as Eye } from './Eye.svg';
 export { default as EyeClose } from './EyeClose.svg';
 export { default as Date } from './Date.svg';
+export { default as AddressBook } from './AddressBook.svg';
 //# sourceMappingURL=index.d.ts.map
