b34407360cae299e0d741335bd221397642f7b9d
Natxo Cabré <cabre@adobe.com>
2024-08-14 14:58:29 +0200

    converted double quotes to single quotes

--- blocks/commerce-checkout-order-summary/commerce-checkout-order-summary.js
+++ blocks/commerce-checkout-order-summary/commerce-checkout-order-summary.js
@@ -1,16 +1,16 @@
-import { render as provider } from "@dropins/storefront-cart/render.js";
-import OrderSummary from "@dropins/storefront-cart/containers/OrderSummary.js";
+import { render as provider } from '@dropins/storefront-cart/render.js';
+import OrderSummary from '@dropins/storefront-cart/containers/OrderSummary.js';
 
-import EstimateShipping from "@dropins/storefront-checkout/containers/EstimateShipping.js";
-import { render as checkoutProvider } from "@dropins/storefront-checkout/render.js";
+import EstimateShipping from '@dropins/storefront-checkout/containers/EstimateShipping.js';
+import { render as checkoutProvider } from '@dropins/storefront-checkout/render.js';
 
 export default async function decorate(block) {
-  block.innerHTML = "";
+  block.innerHTML = '';
 
   return provider.render(OrderSummary, {
     slots: {
       EstimateShipping: (esCtx) => {
-        const estimateShippingForm = document.createElement("div");
+        const estimateShippingForm = document.createElement('div');
 
         checkoutProvider.render(EstimateShipping)(estimateShippingForm);
 
