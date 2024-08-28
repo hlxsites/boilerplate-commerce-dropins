58f04fbcf462f8a12bf8bddb632456be86b8c23e
Abrasimov Yaroslav <y.abrasimov@atwix.com>
2024-08-28 17:50:47 +0200

    Fix stylelint errors

--- blocks/commerce-account-sidebar/commerce-account-sidebar.js
+++ blocks/commerce-account-sidebar/commerce-account-sidebar.js
@@ -15,12 +15,12 @@ export default async function decorate(block) {
     };
 
     const menuItemEl = document.createElement('a');
-    menuItemEl.classList.add('commerce-account-sidebar__item');
+    menuItemEl.classList.add('commerce-account-sidebar-item');
     menuItemEl.href = itemConfig.itemLink;
 
     const isItemActive = window.location.href.includes(itemConfig.itemLink);
     if (isItemActive) {
-      menuItemEl.classList.add('commerce-account-sidebar__item--active');
+      menuItemEl.classList.add('commerce-account-sidebar-item-active');
     }
 
     const iconEl = createMenuItemIcon(itemConfig.itemIcon);
@@ -42,21 +42,21 @@ export default async function decorate(block) {
 
 function createMenuItemIcon(iconSource) {
   const iconEl = document.createElement('div');
-  iconEl.classList.add('commerce-account-sidebar__item-icon');
+  iconEl.classList.add('commerce-account-sidebar-item-icon');
   accountRenderer.render(Icon, { source: iconSource, size: 32 })(iconEl);
   return iconEl;
 }
 
 function createMenuItemContent(title, subtitle) {
   const contentEl = document.createElement('div');
-  contentEl.classList.add('commerce-account-sidebar__item-content');
+  contentEl.classList.add('commerce-account-sidebar-item-content');
 
   const titleEl = document.createElement('p');
-  titleEl.classList.add('commerce-account-sidebar__item-title');
+  titleEl.classList.add('commerce-account-sidebar-item-title');
   titleEl.innerText = title;
 
   const subtitleEl = document.createElement('p');
-  subtitleEl.classList.add('commerce-account-sidebar__item-subtitle');
+  subtitleEl.classList.add('commerce-account-sidebar-item-subtitle');
   subtitleEl.innerText = subtitle;
 
   contentEl.appendChild(titleEl);
@@ -66,7 +66,7 @@ function createMenuItemContent(title, subtitle) {
 
 function createMenuItemArrow() {
   const arrowEl = document.createElement('div');
-  arrowEl.classList.add('commerce-account-sidebar__item-arrow');
+  arrowEl.classList.add('commerce-account-sidebar-item-arrow');
   accountRenderer.render(Icon, {
     source: 'ChevronRight',
     size: 32,
