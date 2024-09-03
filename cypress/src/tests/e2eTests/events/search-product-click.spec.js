9aef61ca1fca69af3ca3b7a8d12ee26c65ce5017
Stephen Rugh <rugh@adobe.com>
2024-09-03 13:08:42 -0500

    search-product-click test

--- cypress/src/tests/e2eTests/events/search-product-click.spec.js
+++ cypress/src/tests/e2eTests/events/search-product-click.spec.js
@@ -1,25 +1,22 @@
-it('is sent on search bar product click', (done) => {
-  cy.visit('/');
-  cy.wait(5000);
-  cy.waitForResource('commerce-events-collector.js')
-    .then(() => {
-      cy.window().its('adobeDataLayer').then((adobeDataLayer) => {
-        cy.on('window:unload', (e) => {
-          const targetEventIndex = adobeDataLayer.findIndex(event => event?.event === 'search-product-click');
-          // TODO: this fails because the unload is triggering before the event is pushed.
-          // In reality, this event is actually pushed and visible in kibana.
-          expect(targetEventIndex).to.be.greaterThan(-1);
-          done();
-        });
-        // Wait for search input field to be rendered and clickable
-        cy.get('.nav-search-button').should('be.visible').click();
-        cy.wait(2000);
-        // Type in the search query
-        cy.get('#search').type('shirt');
-        cy.get('.livesearch.product-link', { timeout: 10000 })
-          .then(results => {
-            results[0].click();
+it("is sent on search bar product click", () => {
+  cy.visit("/");
+  cy.waitForResource("commerce-events-collector.js").then(() => {
+    cy.window().then((win) => {
+      cy.spy(win.adobeDataLayer, "push").as("adl");
+      cy.get(".nav-search-button").should("be.visible").click();
+      cy.wait(2000);
+      cy.get("#search").type("shirt");
+      cy.get(".livesearch.product-link", { timeout: 10000 })
+        .first()
+        .click()
+        .then(() => {
+          cy.get("@adl").should((adobeDataLayerPush) => {
+            const targetEventIndex = adobeDataLayerPush.args.findIndex(
+              (event) => event[0]?.event === "search-product-click"
+            );
+            expect(targetEventIndex).to.be.greaterThan(-1);
           });
-      });
+        });
     });
+  });
 });
