a780d790c9b9415e08df32b428c82437e44dc472
Stephen Rugh <rugh@adobe.com>
2024-09-03 14:38:39 -0500

    Adds initiate-checkout and improves product search click

--- cypress/src/tests/e2eTests/events/product-page-view.spec.js
+++ cypress/src/tests/e2eTests/events/product-page-view.spec.js
@@ -1,21 +1,24 @@
+/**
+ * https://github.com/adobe/commerce-events/blob/main/examples/events/product-page-view.md
+ *
+ * Required Contexts: page, storefront, product, shoppingCart
+ */
 it('is sent on product page view/render', () => {
-  // on PDP
   cy.visit('/products/frankie-sweatshirt/MH04');
-  // wait for the collector to start loading, indicating "initial" ACDL contexts and events are
   cy.waitForResource('commerce-events-collector.js')
     .then(() => {
       cy.window().its('adobeDataLayer').then((adobeDataLayer) => {
         const targetEventIndex = adobeDataLayer.findIndex(event => event?.event === 'product-page-view');
         const pageContextIndex = adobeDataLayer.findIndex(event => !!event?.pageContext);
+        const storefrontInstanceContextIndex = adobeDataLayer.findIndex(event => !!event?.storefrontInstanceContext);
         const productContextIndex = adobeDataLayer.findIndex(event => !!event?.productContext);
         const shoppingCartContextIndex = adobeDataLayer.findIndex(event => !!event?.shoppingCartContext);
-        const storefrontInstanceContextIndex = adobeDataLayer.findIndex(event => !!event?.storefrontInstanceContext);
 
         // expected contexts and event are in ACDL
         expect(targetEventIndex).to.be.greaterThan(-1);
         expect(pageContextIndex).to.be.greaterThan(-1);
-        expect(productContextIndex).to.be.greaterThan(-1);
         expect(storefrontInstanceContextIndex).to.be.greaterThan(-1);
+        expect(productContextIndex).to.be.greaterThan(-1);
         // TODO: this context is not pushed because it is "owned" by the cart dropin. See DINT-1656.
         expect(shoppingCartContextIndex).to.be.greaterThan(-1);
       });
--- cypress/src/tests/e2eTests/events/search-request-sent.spec.js
+++ cypress/src/tests/e2eTests/events/search-request-sent.spec.js
@@ -1,7 +1,7 @@
 /**
- * https://github.com/adobe/commerce-events/blob/main/examples/events/search-results-view.md
+ * https://github.com/adobe/commerce-events/blob/main/examples/events/search-request-sent.md
  *
- * Required Contexts: page, storefront, searcResults
+ * Required Contexts: page, storefront, searchInput
  */
 it('is sent on search bar view/render', () => {
   cy.visit('/');
@@ -11,16 +11,16 @@ it('is sent on search bar view/render', () => {
   cy.waitForResource('commerce-events-collector.js')
     .then(() => {
       cy.window().its('adobeDataLayer').then((adobeDataLayer) => {
-        const targetEventIndex = adobeDataLayer.findIndex(event => event?.event === 'search-results-view');
+        const targetEventIndex = adobeDataLayer.findIndex(event => event?.event === 'search-request-sent');
         const pageContextIndex = adobeDataLayer.findIndex(event => !!event?.pageContext);
-        const searchResultsContextIndex = adobeDataLayer.findIndex(event => !!event?.searchResultsContext);
         const storefrontInstanceContextIndex = adobeDataLayer.findIndex(event => !!event?.storefrontInstanceContext);
+        const searchInputContextIndex = adobeDataLayer.findIndex(event => !!event?.searchInputContext);
 
         // expected contexts and event are in ACDL
         expect(targetEventIndex).to.be.greaterThan(-1);
         expect(pageContextIndex).to.be.greaterThan(-1);
         expect(storefrontInstanceContextIndex).to.be.greaterThan(-1);
-        expect(searchResultsContextIndex).to.be.greaterThan(-1);
+        expect(searchInputContextIndex).to.be.greaterThan(-1);
       });
     });
 });
@@ -29,16 +29,16 @@ it('is sent on search results page on view/render', () => {
   cy.visit('/search?q=test');
   cy.waitForResource('commerce-events-collector.js').then(() => {
     cy.window().its('adobeDataLayer').then((adobeDataLayer) => {
-      const targetEventIndex = adobeDataLayer.findIndex(event => event?.event === 'search-results-view');
+      const targetEventIndex = adobeDataLayer.findIndex(event => event?.event === 'search-request-sent');
       const pageContextIndex = adobeDataLayer.findIndex(event => !!event?.pageContext);
       const storefrontInstanceContextIndex = adobeDataLayer.findIndex(event => !!event?.storefrontInstanceContext);
-      const searchResultsContextIndex = adobeDataLayer.findIndex(event => !!event?.searchResultsContext);
+      const searchInputContextIndex = adobeDataLayer.findIndex(event => !!event?.searchInputContext);
 
       // expected contexts and event are in ACDL
       expect(targetEventIndex).to.be.greaterThan(-1);
       expect(pageContextIndex).to.be.greaterThan(-1);
       expect(storefrontInstanceContextIndex).to.be.greaterThan(-1);
-      expect(searchResultsContextIndex).to.be.greaterThan(-1);
+      expect(searchInputContextIndex).to.be.greaterThan(-1);
     });
   });
 });
